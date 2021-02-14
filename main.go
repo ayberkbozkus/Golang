c89199523b57d5a84248a1c59cb5744551ffa8d7
ayberkbozkus <32310900+ayberkbozkus@users.noreply.github.com>
2021-02-15 00:08:01 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+// Package clause
+package main
+
+// Import statement
+import "fmt"
+
+// My Code
+func main() {
+
+	var name, lastname string = "tester", "second name"
+
+	var age int
+	age = 40
+
+	var isMarried bool
+	isMarried = true
+
+	fmt.Println("Hello World", name, lastname, age, isMarried)
+}
