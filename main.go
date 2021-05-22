b72689baaf8e9a968532ff683a04a5936fd26680
ayberkbozkus <32310900+ayberkbozkus@users.noreply.github.com>
2021-05-22 14:10:24 +0300

    go study

--- main.go
+++ main.go
@@ -4,16 +4,27 @@ package main
 // Import statement
 import "fmt"
 
+var packVar = "Test"
+
 // My Code
 func main() {
 
-	var name, lastname string = "tester", "second name"
+	var (
+		name, lastname string = "tester", "second name"
+		name1          string = "secondname"
+		age            int    = 40
+		isMarried      bool   = true
+	)
+	height := 17.5
+	var packVar = "Test2"
 
-	var age int
-	age = 40
+	fmt.Println("Hello World", name, name1, lastname, age, isMarried, height)
+	fmt.Printf("%T\n%T\n%T\n", name, age, isMarried)
+	funcVar()
+	fmt.Println(packVar)
+	fmt.Printf("%v = %T", int(height), height)
+}
 
-	var isMarried bool
-	isMarried = true
-
-	fmt.Println("Hello World", name, lastname, age, isMarried)
+func funcVar() {
+	fmt.Println(packVar)
 }
