6adf42022db56b1fe6984fe6578e4112ec87fbf1
ayberkbozkus <32310900+ayberkbozkus@users.noreply.github.com>
2021-06-26 15:14:31 +0300

    maps example

---
+++ maps.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func main() {
+	myMap := map[string]int {
+		"Ahmet": 40,
+		"Ayşe": 17,
+		"Selim": 14,
+		"Mustafa": 70,
+	}
+	fmt.Println(myMap)
+	fmt.Println(myMap["Ahmet"], myMap["Selim"])
+
+	isMarried := map[string]bool {
+		"Ahmet": true,
+		"Ayşe": false,
+		"Selim": false,
+		"Mustafa": false,
+	}
+	fmt.Println(isMarried)
+
+	myMap2 := make(map[string]int)
+
+	fmt.Println(myMap2)
+
+	studentGrades := map[string]int {
+		"Arin": 80,
+		"Ahmet": 29,
+		"Selim": 72,
+		"Ayşe": 77,
+		"Çınar": 0,
+	}
+
+	_, ok := studentGrades["Elis"]
+	fmt.Println(ok)
+	_, ok = studentGrades["arin"]
+	fmt.Println(ok)
+
+	fmt.Println(studentGrades)
+	studentGrades["Mahmut"] = 55
+	fmt.Println(studentGrades)
+
+	delete(studentGrades, "Selim")
+	fmt.Println(studentGrades)
+
+	sg := studentGrades // pass by reference
+	fmt.Println(len(studentGrades))
+	fmt.Println(len(sg))
+	delete(sg, "Arin")
+	fmt.Println(len(studentGrades))
+	fmt.Println(len(sg))
+
+
+	for k, v := range studentGrades{
+		fmt.Println(k,v)
+	}
+}
