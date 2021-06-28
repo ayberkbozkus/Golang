df4c2040566b7db4c4a80ef5d0fe1a431c711982
ayberkbozkus <32310900+ayberkbozkus@users.noreply.github.com>
2021-06-29 00:30:46 +0300

    Polymorphism example with rectangle

--- rectangle.go
+++ rectangle.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type rectangle struct {
 	a, b float64
@@ -18,6 +21,26 @@ func (r rectangle) circumference() float64 {
 	return 2 * (r.a + r.b)
 }
 
+type circle struct {
+	r float64
+}
+
+func (c circle) display(){
+	fmt.Println("R: ", c.r)
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+func (c circle) circumference() float64 {
+	return 2 * math.Pi * c.r
+}
+
+func (c circle) diameter() float64 {
+	return 2 * c.r
+}
+
 type shape interface {
 	area() float64
 	circumference() float64
@@ -31,11 +54,23 @@ func interfacefunc (i shape) {
 	fmt.Println()
 }
 
+func printArea(shapes ...shape){
+	for _, shape := range shapes{
+		fmt.Println("Area: ", shape.area())
+	}
+}
+
 func main(){
 	r1 := rectangle{3,8}
+	c1 := circle{3}
+	r2 := rectangle{10,10}
 	r1.display()
 	fmt.Println("Area: ", r1.area())
 	fmt.Println("Circumference: ", r1.circumference())
 	fmt.Println(" -------------- ")
 	interfacefunc(r1)
+	fmt.Println(" -------------- ")
+	interfacefunc(c1)
+	fmt.Println(" -------------- ")
+	printArea(r1,c1,r2)
 }
