55d44ec5ea773316f90ac424a823fef5c542930c
ayberkbozkus <32310900+ayberkbozkus@users.noreply.github.com>
2021-06-29 00:15:51 +0300

    Interface example

--- rectangle.go
+++ rectangle.go
@@ -3,19 +3,32 @@ package main
 import "fmt"
 
 type rectangle struct {
-	a, b int
+	a, b float64
 }
 
 func (r rectangle) display(){
 	fmt.Println("Side lengths: ", r.a*2, " - ", r.b*2)
 }
 
-func (r rectangle) area() int {
+func (r rectangle) area() float64 {
 	return r.a * r.b
 }
 
-func (r rectangle) circumference() int {
+func (r rectangle) circumference() float64 {
 	return 2 * (r.a + r.b)
+}
+
+type shape interface {
+	area() float64
+	circumference() float64
+}
+
+func interfacefunc (i shape) {
+	fmt.Println(i)
+	fmt.Println(i.area())
+	fmt.Println(i.circumference)
+	fmt.Printf("%T", i)
+	fmt.Println()
 }
 
 func main(){
@@ -23,4 +36,6 @@ func main(){
 	r1.display()
 	fmt.Println("Area: ", r1.area())
 	fmt.Println("Circumference: ", r1.circumference())
+	fmt.Println(" -------------- ")
+	interfacefunc(r1)
 }
