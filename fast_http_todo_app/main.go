b2fe3538d70a27793be2c3aa00297b63ee76c6e8
ayberkbozkus <32310900+ayberkbozkus@users.noreply.github.com>
2021-07-25 18:01:53 +0300

    todo test

--- fast_http_todo_app/main.go
+++ fast_http_todo_app/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
     "github.com/gofiber/fiber/v2"
+    "fmt"
 )
+
+func helloWorld(c *fiber.Ctx) error {
+    return c.SendString("Hello, World 👋!")
+}
+
+func apiFunc(c *fiber.Ctx) error {
+    msg := fmt.Sprintf("✋ %s", c.Params("*"))
+    return c.SendString(msg) // => ✋ register
+}
 
 func main() {
     app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
-    })
+    app.Get("/", helloWorld)
+
+    app.Get("/api/*", apiFunc)
 
     app.Listen(":3000")
 }
